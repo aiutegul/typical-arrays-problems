d3de2c31bf676f7e6acd24a9db2943673b6ecf73
Aidos <Aidos@DESKTOP-7GOE6P4>
2021-03-14 00:42:21 +0600

    init: implement all functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array)
+    return 0;
+  else if(array.length === 0)
+    return 0;
+  else if (array.length === 1) 
+    return array[0];
+  else {
+    let min = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] < min)
+        min = array[i]; 
+    }
+    return min;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array)
+    return 0;
+  else if(array.length === 0)
+    return 0;
+  else if (array.length === 1) 
+    return array[0];
+  else {
+    let max = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (array[i] > max)
+        max = array[i]; 
+    }
+    return max;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array)
+    return 0;
+  else if(array.length === 0)
+    return 0;
+  else if (array.length === 1) 
+    return array[0];
+  else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum / array.length;
+  }
 }
